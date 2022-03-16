6a5252528436909cee3bfaa7bc04dd342c351833
Amirshoja <ashojakhani@gmail.com>
2022-03-16 14:38:41 -0700

    Added app.r

---
+++ app.R
@@ -0,0 +1,103 @@
+library(dash)
+library(dashCoreComponents)
+library(dashBootstrapComponents)
+library(here)
+
+# path to current work directory
+dir_path <- dirname(rstudioapi::getSourceEditorContext()$path)
+
+# load external functions
+source(here(dir_path, 'src/queries.R'))
+source(here(dir_path, 'src/component_app_header.R'))
+source(here(dir_path, 'src/component_topgdp.R'))
+source(here(dir_path, 'src/component_countries_kpis.R'))
+source(here(dir_path, 'src/component_continent_kpis.R'))
+
+app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
+
+# Sync continents and countries in filter
+app$callback(
+  output("country-selector", 'options'),
+  list(input("continent-selector", 'value')),
+  function(selected_continent) {
+    return(create_countries_list(selected_continent))
+  }
+)
+
+# Update Top GDP plot
+app$callback(
+  output('top-gdp-plot', 'figure'),
+  list(input('continent-selector', 'value'),
+       input("country-selector", 'value')),
+  function(selected_continent, selected_countries) {
+    return(plot_topGdp(selected_continent, selected_countries))
+  })
+
+# Update Country KPIs
+app$callback(
+  list(
+    output('gdp_country', 'children'),
+    output('pop_country', 'children'),
+    output('lifeexp_country', 'children'),
+    output('gdp_value', 'children'),
+    output('pop_value', 'children'),
+    output('lifeexp_value', 'children')
+  ),
+  list(
+    input('continent-selector', 'value'),
+    input('country-selector', 'value'),
+    input('country-kpi-type', 'value')
+  ),
+  function(selected_continent, selected_countries, country_kpi_type) {
+    return(plot_countries_kpis(selected_continent, selected_countries, country_kpi_type))
+  })
+
+# Update Continent KPIs
+app$callback(
+  list(
+    output('continent-mean-gdp', 'children'),
+    output('continent-mean-pop', 'children'),
+    output('continent-mean-lifeexp', 'children')
+  ),
+  list(
+    input('continent-selector', 'value'),
+    input('country-selector', 'value')),
+  function(selected_continent, selected_countries) {
+    return(plot_continent_kpis(selected_continent, selected_countries))
+  })
+
+
+app |> set_layout(
+  list(
+    dbcContainer(dbcRow(app_header), fluid = TRUE),
+    dbcContainer(
+      list(
+        dbcRow(
+          list(
+            dbcCol(
+              list(
+                dbcRow(dbcCol(h1('map'), width = 12)),
+                dbcRow(dbcCol(h1(countries_kpi_cards_div), width = 12)),
+                dbcRow(dbcCol(h1(continent_kpi_cards), width = 12))
+              ),
+              width = 6
+            ),
+            dbcCol(
+              list(
+                dbcRow(dbcCol(h1('timeseries_card'), width = 12)),
+                dbcRow(dbcCol(h1(top_gdp_card), width = 12)),
+                dbcRow(dbcCol(h1("gdp_exp_card"), width = 12))
+              ),
+              width = 6
+            )
+          )
+        )
+      ),
+      className = "mb-3"
+    )
+  )
+)
+
+
+# Run the app
+app %>% run_app()
