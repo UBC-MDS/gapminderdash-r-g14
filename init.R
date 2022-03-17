d32fc1ac659037b959a17a620324d1c18212955e
Luke Collins <luke.a.collins@outlook.com>
2022-03-17 12:35:56 -0700

    fix: add dash html components to dependencies

--- init.R
+++ init.R
@@ -10,6 +10,6 @@ options(repos=r)
 
 # packages go here
 install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes',
-                   'dashCoreComponents', 'dashBootstrapComponents', 
+                   'dashCoreComponents', 'dashBootstrapComponents', 'dashHtmlComponents' 
                    'plotly', 'tidyverse', 'gapminder', 'ggplot2'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
