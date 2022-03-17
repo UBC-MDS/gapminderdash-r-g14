4544822f3f36a71a3f000ac557d84b692344e908
Luke Collins <luke.a.collins@outlook.com>
2022-03-17 13:30:35 -0700

    Create component_gdplifeexp.R

---
+++ src/component_gdplifeexp.R
@@ -0,0 +1,18 @@
+library(dash)
+library(dashCoreComponents)
+library(dashBootstrapComponents)
+
+# path to current work directory
+dir_path <- dirname(rstudioapi::getSourceEditorContext()$path)
+
+# load external functions
+source(here(dir_path, 'src/plotting.R'))
+
+gdp_lifeexp_card <- dbcCard(
+  dbcCardBody(
+    list(
+      h4("Life Expectancy vs. GDP", className = "card-title"),
+      dccGraph(id="gdp_lifeexp_plot")
+    )
+  )
+)
